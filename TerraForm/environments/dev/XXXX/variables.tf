098e22b99f54e0433d2a9f452e079ba97f87a022
Kevin Bickmore <Kevin.Bickmore@vca.com>
2019-04-08 10:35:33 -0700

    added data sources

--- TerraForm/environments/dev/XXXX/variables.tf
+++ TerraForm/environments/dev/XXXX/variables.tf
@@ -10,3 +10,15 @@ variable "subnet" {}
 variable "IteKeyVaultName" {}
 variable "dsc_endpoint" {}
 variable "dsc_key" {}
+
+## Data Sources
+data "azurerm_resource_group" "IteResourceGroup" {
+  name = "${var.IteResourceGroup}"
+}
+
+data "azurerm_client_config" "current" {}
+
+data "azurerm_key_vault" "ITEKeyVault" {
+  name                = "${var.IteKeyVaultName}"
+  resource_group_name = "${data.azurerm_resource_group.IteResourceGroup.name}"
+}
