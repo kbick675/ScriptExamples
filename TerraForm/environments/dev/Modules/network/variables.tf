f11d684ecaf320d97b1a66c94a72a52d5703b7cf
Kevin Bickmore <Kevin.Bickmore@vca.com>
2019-04-08 10:35:23 -0700

    cleaned up variables

--- TerraForm/environments/dev/Modules/network/variables.tf
+++ TerraForm/environments/dev/Modules/network/variables.tf
@@ -1,28 +1,11 @@
-variable "environment" {
-  type = "string"
+variable "environment" {}
 
-  # dev, prod
-  default = "dev"
-}
+variable "Number" {}
 
-variable "HospNumber" {
-  type    = "string"
-  default = "NNNN"
-}
+variable "ResourceGroupName" {}
 
-variable "ResourceGroupName" {
-  type = "string"
-}
+variable "vNetSpace" {}
 
-variable "vNetSpace" {
-  type    = "string"
-  default = "10.0.0.0/16"
-}
+variable "Subnet" {}
 
-variable "Subnet" {
-  type = "string"
-}
-
-variable "location" {
-  type = "string"
-}
+variable "location" {}
