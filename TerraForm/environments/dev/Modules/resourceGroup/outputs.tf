91dce07cf5ffbbeb69f6648af20512be0fb76d7d
Kevin Bickmore <Kevin.Bickmore@vca.com>
2019-04-08 10:35:44 -0700

    added outputs

--- TerraForm/environments/dev/Modules/resourceGroup/outputs.tf
+++ TerraForm/environments/dev/Modules/resourceGroup/outputs.tf
@@ -1,1 +1,11 @@
+output "resourceGroupId" {
+  value = "${azurerm_resource_group.ResourceGroup.id}"
+}
 
+output "resourceGroupName" {
+  value = "${azurerm_resource_group.ResourceGroup.name}"
+}
+
+output "resourceGroupLocation" {
+  value = "${azurerm_resource_group.ResourceGroup.location}"
+}
